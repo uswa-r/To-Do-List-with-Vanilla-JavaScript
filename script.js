b2e40c7a690b18f3a11d24698bc877cb3b2a19bb
uswa.r <uswa.r@outlook.com>
2022-09-11 04:26:02 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+const form = document.querySelector(".grocery-form");
+const alert = document.querySelector(".alert");
+const grocery = document.getElementById("grocery");
+const submitBtn = document.querySelector(".submit-btn");
+const container = document.querySelector(".grocery-container");
+const list = document.querySelector(".grocery-list");
+const clearBtn = document.querySelector(".clear-btn");
