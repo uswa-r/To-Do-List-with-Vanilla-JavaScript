65659088caf0b30f1cce5d6bcb4d48af75529e9c
uswa.r <uswa.r@outlook.com>
2022-09-13 01:29:40 +0500

    .

--- script.js
+++ script.js
@@ -5,3 +5,40 @@ const submitBtn = document.querySelector(".submit-btn");
 const container = document.querySelector(".grocery-container");
 const list = document.querySelector(".grocery-list");
 const clearBtn = document.querySelector(".clear-btn");
+//edit opts:
+let editELement;
+let editFlag = false;
+let editID = "";
+//eventListeners to submit form:
+form.addEventListener('submit', addItem);
+//funcs:
+function addItem(e) {
+    e.preventDefault();
+    const value = grocery.value;
+    const id = new Date().getTime().toString();
+    console.log(id);
+    //adding condition:
+    if (value && !editFlag) {
+        console.log('add item to the list')
+    }
+    //2nd condition:
+    else if (value &&
+        editFlag) {
+
+        console.log('editting')
+    }
+    //3rd condition:
+    else {
+        displayAlert("please enter anything in the bar below:", "danger");
+    }
+}
+//display alert:
+function displayAlert(text, action) {
+    alert.textContent = text;
+    alert.classList.add('alert-${action}');
+}
+//remove alert:
+setTimeout(function() {
+    alert.textContent = "";
+    alert.classList.remove('alert-${action}');
+}, 1000)
